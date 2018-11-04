10c8241981f0f1cd44384c1b0c0f41e33c4f8678
Deng <d.vay@qq.com>
2018-11-05 00:11:21 +0800

    Update stacker.py and classifiers.py

--- bow_model/classifiers.py
+++ bow_model/classifiers.py
@@ -7,7 +7,10 @@ from xgboost.sklearn import XGBClassifier
 from sklearn.svm import SVC, LinearSVC
 from sklearn.model_selection import train_test_split, GridSearchCV, StratifiedKFold
 from sklearn.externals import joblib
-from sklearn.metrics import f1_score, accuracy_score
+from sklearn.metrics import f1_score, accuracy_score, precision_score, recall_score, classification_report
+from sklearn.preprocessing import MultiLabelBinarizer, LabelBinarizer
+from sklearn.multiclass import OneVsRestClassifier
+from sklearn.linear_model import LogisticRegression
 from time import time
 import numpy as np
 import pandas as pd
@@ -16,6 +19,7 @@ from preprocessing.prepare_data import generate_vectors, id2sub
 from utils.path_util import from_project_root
 from utils.proba_util import predict_proba
 from utils.evaluate import evaluate
+from bow_model.classes import LinearSVCP
 
 N_JOBS = 6
 N_CLASSES = 10
@@ -46,47 +50,63 @@ def validate_clf(clf, X, y, scoring='f1_micro'):
     print('averaged %s on val set: %f\n' % (scoring, test_scores.mean()))
 
 
-def calc_f1(clf, X_val, val_url, senti=None, threshold=0.139):
+def calc_metrics(y_true, y_pred, y_probas=None):
     """ validating clf according metrics described in info page
 
     Args:
-        clf: classifier
-        X_val: val data
-        val_url: url to validation csv file
-        senti: sentiment of each content in val data
-        threshold:  threshold to be predicted as subject
+        y_true: real labels, shape = (n_samples, n_labels)
+        y_pred: predicted labels, shape = (n_samples, ), (n_samples, n_labels)
+        y_probas: predicted probabilities of each label, shape = (n_samples, )
 
     """
-    pred = predict_proba(clf, X_val).values
-    true = pd.read_csv(val_url, usecols=list(map(str, range(10)))).values
-    pred[pred < threshold] = 2
-    pred[pred < 2] = 0
-    tp, fp, fn = 0, 0, 0
-    if senti is None:
-        senti = np.zeros(pred.shape[0])
-    for i in range(pred.shape[0]):
-        no_result = 1
-        for j in range(pred.shape[1]):
-            if true[i][j] > 1 and pred[i][j] > 1:  # both == 2
-                continue
-            if true[i][j] == pred[i][j]:  # true == pred, correctly predicted
-                tp += 1
-            if pred[i][j] < 2:
-                no_result = 0
-                fp += 1
-            if true[i][j] < 2:
-                fn += 1
-        fp += no_result
+    if len(y_pred.shape) == 1:
+        mlb = MultiLabelBinarizer().fit([range(10)])
+        y_pred = mlb.transform(y_pred.reshape(-1, 1))
 
-    fn -= tp
-    fp -= tp
+    if len(y_pred.shape) == 3:
+        y_pred = y_pred[:, :, 1].T
 
-    print(tp, fp, fn, tp + fn)
-    recall = tp / (tp + fn)
-    precision = tp / (tp + fp)
-    micro_f1 = 2 * recall * precision / (recall + precision)
-    print("metrics on validation set: \n recall = %f, precision = %f, micro_f1 = %f\n"
-          % (recall, precision, micro_f1))
+    print("metrics before fill non result samples:")
+    print(" precision on subject: %f" % precision_score(y_true, y_pred, average='micro'))
+    print(" recall    on subject: %f" % recall_score(y_true, y_pred, average='micro'))
+    print(" micro f1  on subject: %f\n" % f1_score(y_true, y_pred, average='micro'))
+
+    # if y_probas gave, make sure each sample has at least 1 label
+    if y_probas is not None:
+        for i, row in enumerate(y_pred):
+            if sum(row) < 1:
+                row[y_probas[i].argmax()] = 1
+
+    print("details after fill non result with argmax:\n")
+    names = ['价格', '配置', '操控', '舒适', '油耗', '动力', '内饰', '安全', '空间', '外观']
+    print(classification_report(y_true, y_pred, target_names=names, digits=6))
+
+    # pred[pred < threshold] = 2
+    # pred[pred < 2] = 0
+    # tp, fp, fn = 0, 0, 0
+    # for i in range(pred.shape[0]):
+    #     no_result = 1
+    #     for j in range(pred.shape[1]):
+    #         if true[i][j] > 1 and pred[i][j] > 1:  # both == 2
+    #             continue
+    #         if true[i][j] == pred[i][j]:  # true == pred, correctly predicted
+    #             tp += 1
+    #         if pred[i][j] < 2:
+    #             no_result = 0
+    #             fp += 1
+    #         if true[i][j] < 2:
+    #             fn += 1
+    #     fp += no_result
+    #
+    # fn -= tp
+    # fp -= tp
+    #
+    # print(tp, fp, fn, tp + fn)
+    # recall = tp / (tp + fn)
+    # precision = tp / (tp + fp)
+    # micro_f1 = 2 * recall * precision / (recall + precision)
+    # print("metrics on validation set: \n recall = %f, precision = %f, micro_f1 = %f\n"
+    #       % (recall, precision, micro_f1))
 
 
 def init_clfs():
@@ -142,11 +162,12 @@ def train_clfs(clfs, X, y, test_size=0.2, validating=False, random_state=None):
     return clfs
 
 
-def validate(pkl_url=None):
+def validate(pkl_url=None, cv=False):
     """ do validating
 
         Args:
             pkl_url: load data from pickle file, set to None to generate data instantly
+            cv: do cross validation or not
 
     """
     clfs = init_clfs()
@@ -164,14 +185,19 @@ def validate(pkl_url=None):
                                        multilabel_out=False, label_col='subjects', only_single=True, shuffle=True)
 
     print(X.shape, y.shape, X_val.shape)
-    train_clfs(clfs, X, y, validating=True, random_state=RANDOM_STATE)
-    for name in clfs:
+    if cv:
+        train_clfs(clfs, X, y, validating=True, random_state=RANDOM_STATE)
+
+    for name, clf in clfs.items():
         print("metrics of %s classifier:" % name)
-        clfs[name].fit(X, y)
-        calc_f1(clfs[name], X_val, val_url=val_url, senti=None, threshold=0.139)
+        clf.fit(X, y)
+        y_true = pd.read_csv(val_url, usecols=list(map(str, range(10)))).values < 2
+        y_pred = clf.predict(X_val)
+        y_probas = predict_proba(clf, X_val)
+        calc_metrics(y_true, y_pred, y_probas)
 
 
-def gen_10bi_probas(train_url, test_url, validating=False):
+def gen_10bi_result(train_url, test_url, validating=False):
     """
 
     Args:
@@ -185,70 +211,109 @@ def gen_10bi_probas(train_url, test_url, validating=False):
     """
     tdf = pd.read_csv(test_url)['content_id']
     n_samples = len(tdf)
-    probas = np.empty(shape=(n_samples, 0))
+    y_probas = np.empty(shape=(n_samples, 0))
+    y_pred = np.empty(shape=(n_samples, 0), dtype=int)
     for col in range(10):
         X, y, X_test = generate_vectors(train_url, test_url, column='article', max_n=3, min_df=2, max_df=0.8,
-                                        max_features=200000, trans_type='dc', sublinear_tf=True, balanced=True,
-                                        multilabel_out=False, label_col=str(col), only_single=False, shuffle=True,
-                                        apply_fun=lambda label: int(label < 2))
+                                        max_features=33333, trans_type='dc', sublinear_tf=True, balanced=True,
+                                        multilabel_out=False, label_col='subjects', only_single=False, shuffle=True,
+                                        apply_fun=lambda label: int(str(col) in label))
         clf = LinearSVC()
         if validating:
             print("validating on subject %s:" % id2sub(col))
             validate_clf(clf, X, y, scoring='f1_micro')
         clf.fit(X, y)
         proba = predict_proba(clf, X_test)[:, 1:2]
-        probas = np.hstack((probas, proba))
-    print(probas[:3, :])
-    return probas
+        y_probas = np.hstack((y_probas, proba))
+        y_pred = np.hstack((y_pred, clf.predict(X_test).reshape(-1, 1)))
+
+    return y_pred, y_probas
 
 
-def generate_result(evaluating=False, use_n_subjects=False):
+def gen_multi_result(X, y, X_test):
     """
 
     Args:
-        evaluating: evaluating use preliminary data
-        use_n_subjects: use n_subjects info
+        validating: do validating
 
     Returns:
 
     """
-    cid_list = pd.read_csv(from_project_root('data/submit_example_2.csv'))['content_id'].tolist()
+    clf = OneVsRestClassifier(LogisticRegression(solver='liblinear'))
+    # clf = OneVsRestClassifier(LinearSVCP())
+    # clf = OneVsRestClassifier(XGBClassifier())
+    if len(y.shape) == 1:
+        y = MultiLabelBinarizer().fit([range(10)]).transform(y.reshape(-1, 1))
+    clf.fit(X, y)
+    y_pred = clf.predict(X_test)
+    y_probas = clf.predict_proba(X_test)
+    return y_pred, y_probas
+
+
+def generate_result(evaluating=False, use_n_subjects='pred'):
+    """
+
+    Args:
+        evaluating: evaluating use preliminary data
+        use_n_subjects: use n_subjects info, 'gold', 'pred' or 'one'
+
+    Returns:
+
+    """
     train_url = from_project_root("data/train_2_ex.csv")
     test_url = from_project_root("data/test_public_2v3_ex.csv")
     if evaluating:
         train_url = from_project_root("data/preliminary/train_ex.csv")
-        test_url = from_project_root("data/preliminary/test_public_ex.csv")
+        test_url = from_project_root("data/preliminary/test_gold_ex.csv")
+        senti = joblib.load(from_project_root("data/vector/stacked_one_XyX_val_32_sentiment.xgb.result.pk"))
 
     X, y, X_test = generate_vectors(train_url, test_url, column='article', max_n=3, min_df=3, max_df=0.8,
-                                    max_features=20000, trans_type='dc', sublinear_tf=True, balanced=True,
+                                    max_features=33333, trans_type='dc', sublinear_tf=True, balanced=True,
                                     multilabel_out=False, label_col='subjects', only_single=True, shuffle=True)
-    # X, y, X_test = joblib.load(from_project_root('processed_data/vector/stacked_all_XyX_test_48_subjects.pk'))
+    X, y, X_test = joblib.load(from_project_root('data/vector/stacked_one_XyX_val_32_subjects.pk'))
 
-    # clf = LinearSVC()
-    # clf.fit(X, y)
-    # probas = predict_proba(clf, X_test)
-    probas = gen_10bi_probas(train_url, test_url, validating=True)
-    cids = pd.read_csv(test_url, usecols=['content_id']).values.ravel()
+    clf = LinearSVC()
+    clf.fit(X, y)
+    # pred, probas = clf.predict(X_test), predict_proba(clf, X_test)
+    # pred, probas = gen_10bi_result(train_url, test_url, validating=True)
+    pred, probas = gen_multi_result(X, y, X_test)
     result_df = pd.DataFrame(columns=["content_id", "subject", "sentiment_value", "sentiment_word"])
-
+    cids = pd.read_csv(test_url, usecols=['content_id']).values.ravel()
     for i, cid in enumerate(cids):
-        k = cid_list.count(cid) if use_n_subjects else 1
+        k = 1
+        if use_n_subjects == 'gold':
+            cid_list = pd.read_csv(from_project_root('data/submit_example_2.csv'))['content_id'].tolist()
+            k = cid_list.count(cid)
+        elif use_n_subjects == 'pred':
+            k = max(1, pred[i].sum())
         for j in probas[i].argsort()[-k:]:
-            result_df = result_df.append({'content_id': cid, 'subject': id2sub(j), 'sentiment_value': '0'},
+            senti_val = senti[i] if evaluating else 0
+            result_df = result_df.append({'content_id': cid, 'subject': id2sub(j), 'sentiment_value': senti_val},
                                          ignore_index=True)
-        if k == 0:
-            result_df = result_df.append({'content_id': cid}, ignore_index=True)
 
-    save_url = from_project_root('data/result/bdc_0.6277.csv')
+    save_url = from_project_root('data/result/tmp.csv')
     result_df.to_csv(save_url, index=False)
     if evaluating:
+        y_true = pd.read_csv(test_url, usecols=list(map(str, range(10)))).values < 2
+        calc_metrics(y_true, pred, probas)
+        print("metrics on %s subjects: " % use_n_subjects)
         evaluate(save_url, use_senti=False)
         evaluate(save_url, use_senti=True)
 
 
+def gen_senti_result(pkl_url):
+    X, y, X_test = joblib.load(pkl_url)
+    clf = XGBClassifier()
+    clf.fit(X, y)
+    senti = clf.predict(X_test)
+    joblib.dump(senti, pkl_url.replace('.pk', '.xgb.result.pk'))
+
+
 def main():
-    # validate()
-    generate_result(evaluating=True)
+    # pkl_url = from_project_root("data/vector/stacked_one_XyX_val_32_sentiment.pk")
+    # validate(pkl_url=pkl_url)
+    generate_result(evaluating=True, use_n_subjects='pred')
+    # gen_senti_result(pkl_url)
     pass
 
 
--- bow_model/stacker.py
+++ bow_model/stacker.py
@@ -7,8 +7,12 @@ from os.path import exists
 from sklearn.model_selection import StratifiedKFold
 from sklearn.metrics import f1_score, accuracy_score
 from sklearn.svm import LinearSVC
+from sklearn.linear_model import LogisticRegression
+from sklearn.multiclass import OneVsRestClassifier
+from sklearn.preprocessing import MultiLabelBinarizer
 from sklearn.externals import joblib
 from sklearn.base import clone
+from xgboost.sklearn import XGBClassifier
 import scipy as sp
 import numpy as np
 import pandas as pd
@@ -16,13 +20,15 @@ import pandas as pd
 from utils.path_util import from_project_root, basename
 from preprocessing.prepare_data import generate_vectors
 from utils.proba_util import predict_proba
+from bow_model.classes import LinearSVCP
 
 N_CLASSES = 10
-RANDOM_STATE = 233
+RANDOM_STATE = 2333
 DROP_WORDS = 0
-N_JOBS = 10
+N_JOBS = 7
 CV = 5
 LABEL_COL = 'subjects'
+ONLY_SINGLE = True
 
 
 def load_params():
@@ -94,8 +100,9 @@ def load_params():
 
 
 def run_parallel(index, train_url, test_url, params, clf, n_splits, random_state,
-                 use_proba=False, verbose=False, drop_words=0.):
+                 use_proba=False, verbose=False, drop_words=0., only_single=True):
     """ for run cvs parallel
+
     Args:
         index: index to know which cv it belongs to
         train_url: train data url
@@ -107,11 +114,14 @@ def run_parallel(index, train_url, test_url, params, clf, n_splits, random_state
         use_proba: True to predict probabilities of labels instead of labels
         verbose: True to print more info
         drop_words: drop_words for generate_vectors
+        only_single: only single for generate_vectors
+
     Returns:
+        index, y_probas, y_test_probas
     """
 
-    X, y, X_test = generate_vectors(train_url, test_url, drop_words=drop_words, verbose=verbose,
-                                    label_col=LABEL_COL, shuffle=False, **params)
+    X, y, X_test = generate_vectors(train_url, test_url, drop_words=drop_words, verbose=verbose, label_col=LABEL_COL,
+                                    shuffle=False, only_single=only_single, **params)
     if not sp.sparse.isspmatrix_csr(X):
         X = sp.sparse.csr_matrix(X)
 
@@ -124,10 +134,10 @@ def run_parallel(index, train_url, test_url, params, clf, n_splits, random_state
         y_train, y_cv = y[train_index], y[cv_index]
         clf.fit(X_train, y_train)
         y_pred[cv_index] = clf.predict(X_cv).reshape(-1, 1)
-        y_pred_proba[cv_index] = clf._predict_proba_lr(X_cv)
+        y_pred_proba[cv_index] = clf.predict_proba(X_cv)
         print("%d/%d cv macro f1 of params set #%d:" % (ind + 1, n_splits, index),
               f1_score(y_cv, y_pred[cv_index], average='macro'))
-        y_test_pred_proba += clf._predict_proba_lr(X_test)
+        y_test_pred_proba += clf.predict_proba(X_test)
     print("#%d macro f1: " % index, f1_score(y, y_pred, average='macro'))
 
     y_test_pred = clf.predict(X_test).reshape(X_test.shape[0], 1)
@@ -138,7 +148,7 @@ def run_parallel(index, train_url, test_url, params, clf, n_splits, random_state
 
 
 def feature_stacking(train_url, test_url, n_splits=CV, random_state=None, use_proba=False, verbose=False,
-                     drop_words=0.):
+                     drop_words=0., only_single=True):
     """
     Args:
         train_url: url to original train data
@@ -148,18 +158,21 @@ def feature_stacking(train_url, test_url, n_splits=CV, random_state=None, use_pr
         use_proba: True to predict probabilities of labels instead of labels
         verbose: True to print more info
         drop_words: drop_words for run_parallel
+        only_single: only use single label data
     Returns:
         X, y, X_test
     """
 
-    clf = LinearSVC()
+    clf = LinearSVCP()
     # test_url = None
-    X, y, X_test = generate_vectors(train_url, test_url, shuffle=False, sublinear_tf=False)  # for X.shape
+    X, y, X_test = generate_vectors(train_url, test_url, only_single=only_single, shuffle=False,
+                                    sublinear_tf=False, label_col=LABEL_COL)  # for X.shape and y
 
     params_list = load_params()
     parallel = joblib.Parallel(n_jobs=N_JOBS, verbose=True)
     rets = parallel(joblib.delayed(run_parallel)(
-        ind, train_url, test_url, params, clone(clf), n_splits, random_state, use_proba, verbose, drop_words
+        ind, train_url, test_url, params, clone(clf), n_splits, random_state,
+        use_proba, verbose, drop_words, only_single
     ) for ind, params in enumerate(params_list))
     rets = sorted(rets, key=lambda x: x[0])
 
@@ -194,6 +207,48 @@ def model_stacking_from_pk(model_urls):
         X_test = np.append(X_test, X_test_a, axis=1)
 
     return X, y, X_test
+
+
+def gen_multi_data_for_stacking(n_splits=5, random_state=233):
+    clf = OneVsRestClassifier(LogisticRegression(solver='liblinear'))
+    X_one, _, X_test = joblib.load(from_project_root('data/vector/stacked_one_XyX_test_32_subjects.pk'))
+    _, _, X_multi = joblib.load(from_project_root('data/vector/stacked_one_XyX_multi_32_subjects.pk'))
+
+    train_df = pd.read_csv(from_project_root("data/train_2_ex.csv"))
+    skf = StratifiedKFold(n_splits=n_splits, shuffle=bool(random_state), random_state=random_state)
+    y = MultiLabelBinarizer().fit_transform(train_df['subjects'].apply(str.split))
+
+    one_ind = train_df['n_subjects'] == 1
+    multi_ind = train_df['n_subjects'] > 1
+    y_for_split = train_df['subjects'][one_ind].values.astype(int)
+    y_one = y[one_ind]
+
+    y_proba = np.zeros((len(train_df), 10))
+    y_pred_one = np.zeros((X_one.shape[0], 10))  # for printing score of each fold
+    y_proba_one = np.zeros((X_one.shape[0], 10))
+    y_test_proba = np.zeros((X_test.shape[0], 10))
+    y_proba_multi = np.zeros((X_multi.shape[0], 10))
+
+    for ind, (train_index, cv_index) in enumerate(skf.split(X_one, y_for_split)):  # cv split
+        X_train, X_cv = X_one[train_index], X_one[cv_index]
+        y_train, y_cv = y_one[train_index], y_one[cv_index]
+        clf.fit(X_train, y_train)
+        y_pred_one[cv_index] = clf.predict(X_cv)
+        y_proba_one[cv_index] = predict_proba(clf, X_cv)
+        print("%d/%d cv micro f1 :" % (ind + 1, n_splits),
+              f1_score(y_cv, y_pred_one[cv_index], average='micro'))
+        y_test_proba += predict_proba(clf, X_test)
+        y_proba_multi += predict_proba(clf, X_multi)
+    print("micro f1:", f1_score(y_one, y_pred_one, average='micro'))  # calc micro_f1 score
+
+    y_test_proba /= n_splits  # avg
+    y_proba_multi /= n_splits  # avg
+
+    y_proba[one_ind] = y_proba_one
+    y_proba[multi_ind] = y_proba_multi
+
+    print(y_proba.shape, y.shape, y_test_proba.shape)
+    return y_proba, y, y_test_proba
 
 
 def gen_data_for_stacking(clf, X, y, X_test, n_splits=5, random_state=None):
@@ -264,14 +319,41 @@ def generate_meta_feature(data_url, normalize=True):
     return meta_df
 
 
-def main():
+def gen_feature_stacking_result(gen_type='val'):
+    """ generate feature stacking result data
+
+    Args:
+        gen_type: val or test
+
+    Returns:
+        X, y, X_test
+
+    """
     params = load_params()
     print("len(params) =", len(params))
-    save_url = from_project_root("data/vector/stacked_all_XyX_test_%d_%s.pk" % (len(load_params()), LABEL_COL))
-    train_url = from_project_root("data/train_2_ex.csv")
-    test_url = from_project_root("data//test_public_2v3_ex.csv")
-    joblib.dump(feature_stacking(train_url, test_url, use_proba=True, random_state=RANDOM_STATE, drop_words=DROP_WORDS),
-                save_url)
+    save_url = from_project_root("data/vector/stacked_%s_XyX_%s_%d_%s.pk"
+                                 % (('one' if ONLY_SINGLE else 'all'), gen_type, len(load_params()), LABEL_COL))
+    print("stacking data will be saved at", save_url)
+    if gen_type == 'val':
+        train_url = from_project_root("data/preliminary/train_ex.csv")
+        test_url = from_project_root("data/preliminary/test_gold_ex.csv")
+    elif gen_type == 'test':
+        train_url = from_project_root("data/train_2_ex.csv")
+        test_url = from_project_root("data/test_public_2v3_ex.csv")
+    else:
+        print("error, gen_type should be 'val' or 'test'")
+        return
+
+    joblib.dump(feature_stacking(train_url, test_url, use_proba=True, random_state=RANDOM_STATE,
+                                 drop_words=DROP_WORDS, only_single=ONLY_SINGLE), save_url)
+
+
+def main():
+    gen_feature_stacking_result("val")
+    # X, y, X_test = joblib.load(from_project_root("data/vector/stacked_one_XyX_val_32_sentiment.pk"))
+    # print(X.shape, y.shape, X_test.shape)
+    # gen_data_for_stacking(LinearSVCP(), X, y, X_test, random_state=RANDOM_STATE)
+    pass
 
 
 if __name__ == '__main__':
